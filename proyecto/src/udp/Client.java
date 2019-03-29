71f92303e15f21611ec6df2f27f129b7d2650850
Elisa Boselli <elisaboselli@hotmail.com>
2019-03-29 10:30:01 -0300

    Interactive client server

--- proyecto/src/udp/Client.java
+++ proyecto/src/udp/Client.java
@@ -5,6 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Scanner;
 
 import messages.ClientMessage;
 import messages.ClientMessageType;
@@ -21,29 +22,106 @@ public class Client {
 			int portServer = 6789;
 
 			JSONUtils jsonUtils = new JSONUtils();
+			Scanner scan = new Scanner(System.in);
+			boolean nextOp = true;
 
-			Element elem = new Element(1, 1, 0);
-			ClientMessage msg = new ClientMessage(ClientMessageType.GET, elem);
+			System.out.println("\n---------- CLIENT SERVER ----------\n");
 
-			DatagramPacket request = new DatagramPacket(msg.toJSON().getBytes(), msg.toJSON().length(), hostServer,
-					portServer);
+			while (nextOp) {
+				String operation = getOperationFromInput(scan);
+				int id, value;
+				Element elem = null;
+				ClientMessage msg = null;
 
-			socketUDP.send(request);
+				if (operation.equalsIgnoreCase("get")) {
+					id = getIdFromInput(scan);
+					elem = new Element(id, 0, 0);
+					msg = new ClientMessage(ClientMessageType.GET, elem);
+				}
 
-			byte[] buffer = new byte[1000];
-			DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-			socketUDP.receive(response);
+				if (operation.equalsIgnoreCase("set")) {
+					id = getIdFromInput(scan);
+					value = getValueFromInput(scan);
+					elem = new Element(id, value, 0);
+					msg = new ClientMessage(ClientMessageType.SET, elem);
+				}
 
-			ClientMessage responseMsg = jsonUtils.getClientMessage(response);
+				// elem = new Element(1, 1, 0);
+				// msg = new ClientMessage(ClientMessageType.GET, elem);
 
-			System.out.println("Response: " + responseMsg.toString());
+				DatagramPacket request = new DatagramPacket(msg.toJSON().getBytes(), msg.toJSON().length(), hostServer,
+						portServer);
 
+				socketUDP.send(request);
+
+				byte[] buffer = new byte[1000];
+				DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+				socketUDP.receive(response);
+
+				ClientMessage responseMsg = jsonUtils.getClientMessage(response);
+
+				System.out.println("Response: " + responseMsg.toString());
+
+				nextOp = getNextOperationFromInput(scan);
+
+			}
+			scan.close();
 			socketUDP.close();
+			System.out.println("\n---------- CLOSING CLIENT SERVER ----------\n");
 
 		} catch (SocketException e) {
 			System.out.println("Socket: " + e.getMessage());
 		} catch (IOException e) {
 			System.out.println("IO: " + e.getMessage());
 		}
+	}
+
+	private static String getOperationFromInput(Scanner scan) {
+		String input = "";
+		boolean inputCorrect = false;
+
+		System.out.println("Enter the operation to be performed (GET / SET)");
+		while (!inputCorrect) {
+			input = scan.nextLine();
+			inputCorrect = (input.equalsIgnoreCase("get") || input.equalsIgnoreCase("set"));
+			if (!inputCorrect) {
+				System.out.println("Please, enter a valid operation (GET / SET)");
+			}
+		}
+		return input;
+	}
+
+	private static boolean getNextOperationFromInput(Scanner scan) {
+
+		String input = "";
+		boolean inputCorrect = false;
+		scan.nextLine();
+		System.out.println("Do you want to perform another operation ? (Y / N)");
+		while (!inputCorrect) {
+			input = scan.nextLine();
+			if (input.equalsIgnoreCase("Y")) {
+				return true;
+			} else {
+				if (input.equalsIgnoreCase("N")) {
+					return false;
+				}
+			}
+			if (!inputCorrect) {
+				System.out.println("Please, enter a valid option (Y / N)");
+			}
+		}
+		return false;
+	}
+
+	private static int getIdFromInput(Scanner scan) {
+		System.out.println("Enter the element id");
+		int id = scan.nextInt();
+		return id;
+	}
+
+	private static int getValueFromInput(Scanner scan) {
+		System.out.println("Enter the element new value");
+		int value = scan.nextInt();
+		return value;
 	}
 }
